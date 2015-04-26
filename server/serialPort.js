4639ca7d1a56e32ba9e9a02f666307a55e4620e5
clairezed <clairezuliani@gmail.com>
2015-04-26 13:19:15 +0200

    clean

--- server/serialPort.js
+++ server/serialPort.js
@@ -1,55 +1,55 @@
-var Fiber = Npm.require('fibers');
+// var Fiber = Npm.require('fibers');
 
-var serialPort = new SerialPort.SerialPort("/dev/ttyACM0", {
-        baudrate: 9600,
-        parser: SerialPort.parsers.readline('\r\n')
-    });
-  serialPort.on('open', function() {
-      console.log('Port open');
-  });
-  serialPort.on('data', function(data) {
-    console.log(data);
-    switch(data) {
-      case "1":
-        console.log("click btn");
-        changeRoute();
-        break;
-      default:
-        console.log("def");
-      upTree();
-    }
+// var serialPort = new SerialPort.SerialPort("/dev/ttyACM0", {
+//         baudrate: 9600,
+//         parser: SerialPort.parsers.readline('\r\n')
+//     });
+//   serialPort.on('open', function() {
+//       console.log('Port open');
+//   });
+//   serialPort.on('data', function(data) {
+//     console.log(data);
+//     switch(data) {
+//       case "1":
+//         console.log("click btn");
+//         changeRoute();
+//         break;
+//       default:
+//         console.log("def");
+//       upTree();
+//     }
 
-  });
+//   });
 
 
-function upTree() {
-  Fiber(function() {
-    Trees.update({}, {$inc: {count: +1}});
-    console.log("up");
-  }).run();
-}
+// function upTree() {
+//   Fiber(function() {
+//     Trees.update({}, {$inc: {count: +1}});
+//     console.log("up");
+//   }).run();
+// }
 
-function changeRoute() {
-  Fiber(function() {
-    var currentRoad = Roads.findOne().currentRoad
-    console.log(currentRoad);
-    switch(currentRoad) {
-      case "home":
-        console.log("go explanation");
-        Roads.update({}, {currentRoad: "explanation"});
-        break;
-      case "explanation":
-        console.log("go game");
-        Roads.update({}, {currentRoad: "game"});
-        break;
-      case "game":
-        console.log("go home");
-        Roads.update({}, {currentRoad: "home"});
-        break;
-      default:
-        console.log("def raod");
+// function changeRoute() {
+//   Fiber(function() {
+//     var currentRoad = Roads.findOne().currentRoad
+//     console.log(currentRoad);
+//     switch(currentRoad) {
+//       case "home":
+//         console.log("go explanation");
+//         Roads.update({}, {currentRoad: "explanation"});
+//         break;
+//       case "explanation":
+//         console.log("go game");
+//         Roads.update({}, {currentRoad: "game"});
+//         break;
+//       case "game":
+//         console.log("go home");
+//         Roads.update({}, {currentRoad: "home"});
+//         break;
+//       default:
+//         console.log("def raod");
 
-    }
+//     }
 
-  }).run();
-}
+//   }).run();
+// }
